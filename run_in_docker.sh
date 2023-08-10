128e34f2bceac20d9e1e0367bb23b4fc59b08fcb
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2023-08-10 23:19:16 +0900

    add a comment: how to run this script

--- run_in_docker.sh
+++ run_in_docker.sh
@@ -1,3 +1,4 @@
+# docker build -t goma-quri . --build-arg NB_UID=`id -u` && docker run --rm -it -v $PWD:/work -w /work goma-quri bash run_in_docker.sh
 poetry install
 poetry run pip3 install openfermionpyscf
 poetry run python hweansatz_benchmark.py
